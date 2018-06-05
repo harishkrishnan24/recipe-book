fb4d853a79b3848b480482b13d4892cb6c0fa7dd
Harish Krishnan <harishkrishnan1993@gmail.com>
2018-06-05 23:50:59 +0530

    Added basic component structure

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg'
+    )
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
