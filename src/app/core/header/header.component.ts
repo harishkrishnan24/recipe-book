a23f5632146d1ffb9291b071152d312c719623c8
Harish Krishnan <harishkrishnan1993@gmail.com>
2018-08-11 20:21:41 +0530

    * Added ngrx effects

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,25 +1,32 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Component, OnInit } from '@angular/core';
 // import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 import { DataStorageService } from '../../shared/data-storage.service';
-import { AuthService } from '../../auth/auth.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../store/app.reducers';
+import * as fromAuth from '../../auth/store/auth.reducers';
+import * as AuthActions from '../../auth/store/auth.actions';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService,
-              private authService: AuthService) {
+export class HeaderComponent implements OnInit {
+  authState: Observable<fromAuth.State>;
+  constructor(
+    private dataStorageService: DataStorageService,
+    private store: Store<fromApp.AppState>
+  ) {}
+
+  ngOnInit() {
+    this.authState = this.store.select('auth');
   }
 
   onSaveData() {
-    this.dataStorageService.storeRecipes()
-      .subscribe(
-        (response) => {
-          console.log(response);
-        }
-      );
+    this.dataStorageService.storeRecipes().subscribe(response => {
+      console.log(response);
+    });
   }
 
   onFetchData() {
@@ -27,6 +34,6 @@ export class HeaderComponent {
   }
 
   onLogout() {
-    this.authService.logout();
+    this.store.dispatch(new AuthActions.Logout());
   }
 }
